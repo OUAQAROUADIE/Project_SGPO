b4cd292f498b5a395eb68c6a8452c16cbe52787b
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2023-06-23 15:28:24 +0100

    Update Function.php

--- Function.php
+++ Function.php
@@ -1,56 +1,34 @@
 <?php
-session_start();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="IndexCss.css">
-</head>
-<body id="erreur">
-    <div class="row">
-        <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
-            <img src="Images\1651706866270.jpeg" />
-            <div class="px-2">
-                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" class="justify-content-center">
-                    <div class="form-group">
-                        <label class="sr-only">Username</label>
-                        <input type="text" class="form-control" placeholder="username" name="Username">
-                    </div>
-                    <div class="form-group">
-                        <label class="sr-only">Password</label>
-                        <input type="password" class="form-control" placeholder="password" name="Password">
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block mb-4">Login</button>
-                </form>
-            </div>
-        </div>
-    </div>
+// Connexion à la base de données
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sgpo";
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-</body>
-</html>
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-<?php
-if (!empty($_POST)) {
-    require 'connexion.php';
-    extract($_POST);
-
-    $requete = "SELECT * FROM use6 
-                WHERE Username = '$Username'
-                AND Password = '$Password'";
-
-    $data = $pdo->query($requete)->fetch(PDO::FETCH_OBJ);
-
-    if ($data) {
-        header('location:table.php');
-        exit;
-    } else {
-        header('location:erreur.php');
-        echo "erreur";
-
-    }
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("La connexion à la base de données a échoué : " . $conn->connect_error);
 }
+
+// Récupération des valeurs du formulaire
+$username = $_POST['Username'];
+$password = $_POST['Password'];
+
+// Requête pour vérifier les informations de connexion
+$sql = "SELECT * FROM use6 WHERE Username = '$username' AND Password = '$password'";
+$result = $conn->query($sql);
+
+if ($result->num_rows == 1) {
+    // Informations de connexion valides
+    header('location:table.php');
+} else {
+    // Informations de connexion invalides
+    header('location:erreur.php');
+
+}
+
+// Fermeture de la connexion à la base de données
+$conn->close();
 ?>
